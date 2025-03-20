f9b389dbfa6b05e0200ca63d33d8d304f5b1148d
Osber <osberriojalopez@gmail.com>
2025-03-20 18:47:28 -0400

    implementaciones

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -13,3 +13,8 @@ kotlin {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
+
+dependencies {
+    implementation(project(":domain"))
+    // Otras dependencias, por ejemplo, para networking o base de datos
+}
